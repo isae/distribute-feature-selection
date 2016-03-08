a87ffc8a9817624576a9cd0907bd6880f60869d3
iisaev <iisaev@solanteq.com>
2016-03-08 20:45:15 +0300

    f1 score computation

--- src/main/java/ru/ifmo/ctddev/isaev/SimpleMeLiF.java
+++ src/main/java/ru/ifmo/ctddev/isaev/SimpleMeLiF.java
@@ -1,9 +1,10 @@
 package ru.ifmo.ctddev.isaev;
 
+import ru.ifmo.ctddev.isaev.dataset.DataSet;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -13,20 +14,35 @@ import java.util.TreeSet;
  */
 public class SimpleMeLiF {
     public static void main(String[] args) {
-        new SimpleMeLiF().run();
+        DataSet dataSet = datasetReader.readCsv(args[0]);
+        new SimpleMeLiF(dataSet).run();
     }
 
     protected final Set<Point> visitedPoints = new TreeSet<>();
 
+    private final DataSet dataSet;
+
+    private static final DatasetReader datasetReader = new DatasetReader();
+
+    private static final ScoreCalculator scoreCalculator = new ScoreCalculator();
+
+    public SimpleMeLiF(DataSet dataSet) {
+        this.dataSet = dataSet;
+    }
+
     private static final double DELTA = 0.3;
 
     private void run() {
-
+        performCoordinateDescend(new Point(1, 0, 0, 0));
+        performCoordinateDescend(new Point(0, 1, 0, 0));
+        performCoordinateDescend(new Point(0, 0, 1, 0));
+        performCoordinateDescend(new Point(0, 0, 0, 1));
+        performCoordinateDescend(new Point(1, 1, 1, 1));
     }
 
-    protected Double visitPoint(Point point, File folder, double baseScore) throws IOException {
+    protected Double visitPoint(Point point, double baseScore) {
         if (!visitedPoints.contains(point)) {
-            double score = getAUCScore(folder, point);
+            double score = getF1Score(point);
             visitedPoints.add(new Point(point));
             if (score > baseScore) {
                 return score;
@@ -35,8 +51,8 @@ public class SimpleMeLiF {
         return null;
     }
 
-    protected void makeDescend(File folder, Point point) throws IOException {
-        double baseScore = getAUCScore(folder, point);
+    protected double performCoordinateDescend(Point point) {
+        double baseScore = getF1Score(point);
         //writeResults(folder, baseScore, point.getCoordinates());
         visitedPoints.add(new Point(point));
 
@@ -48,14 +64,14 @@ public class SimpleMeLiF {
             for (int i = 0; i < coordinates.length; i++) {
                 double initCoordValue = coordinates[i];
                 coordinates[i] = initCoordValue + DELTA;
-                Double currentScore = visitPoint(point, folder, baseScore);
+                Double currentScore = visitPoint(point, baseScore);
                 if (currentScore != null) {
                     baseScore = currentScore;
                     smthChanged = true;
                     break;
                 }
                 coordinates[i] = initCoordValue - DELTA;
-                currentScore = visitPoint(point, folder, baseScore);
+                currentScore = visitPoint(point, baseScore);
                 if (currentScore != null) {
                     baseScore = currentScore;
                     smthChanged = true;
@@ -64,10 +80,10 @@ public class SimpleMeLiF {
                 coordinates[i] = initCoordValue;
             }
         }
-
+        return baseScore;
     }
 
-    private double getAUCScore(File folder, Point point) throws IOException {
+    private double getF1Score(Point point) {
         //EvaluateOptimisationPoint.main(new String[] {"start", folder.getAbsolutePath(), String.valueOf(point.getCoordinates()[0]), String.valueOf(point.getCoordinates()[1]), String.valueOf(point.getCoordinates()[2]), String.valueOf(point.getCoordinates()[3])});
         File subFolder = new File(folder.getAbsolutePath() + "//7_AUC_scores_WPCA_SVM//");
         double auc = 0;
