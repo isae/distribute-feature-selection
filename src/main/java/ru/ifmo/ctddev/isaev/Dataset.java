888a56e5813426eb5ecbd95dac5a983954c88757
iisaev <iisaev@solanteq.com>
2016-03-08 16:20:50 +0300

    check for two classes in dataset

--- src/main/java/ru/ifmo/ctddev/isaev/Dataset.java
+++ src/main/java/ru/ifmo/ctddev/isaev/Dataset.java
@@ -13,6 +13,9 @@ public class Dataset {
     private List<Integer> classes;
 
     public Dataset(List<List<Double>> features, List<Integer> classes) {
+        if (!classes.stream().allMatch(i -> i == 0 || i == 1)) {
+            throw new IllegalArgumentException("All classes values should be 0 or 1");
+        }
         this.features = Collections.unmodifiableList(features);
         this.classes = Collections.unmodifiableList(classes);
     }
---
+++ src/main/java/ru/ifmo/ctddev/isaev/FeatureMeasure.java
@@ -0,0 +1,7 @@
+package ru.ifmo.ctddev.isaev;
+
+/**
+ * @author iisaev
+ */
+public interface FeatureMeasure {
+}
