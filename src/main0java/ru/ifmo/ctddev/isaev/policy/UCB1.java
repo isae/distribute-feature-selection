bffdb5ea92fd57f556f2aeda3835692e9812b44c
iisaev <iisaev@solanteq.com>
2016-05-23 10:28:51 +0300

    minor

---
+++ src/main0java/ru/ifmo/ctddev/isaev/policy/UCB1.java
@@ -0,0 +1,46 @@
+package ru.ifmo.ctddev.isaev.policy;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.log;
+import static java.lang.Math.sqrt;
+
+
+/**
+ * @author iisaev
+ */
+@ThreadSafe
+public class UCB1 extends BanditStrategy {
+
+    private int tries = 0;
+
+    public UCB1(int arms) {
+        super(arms);
+    }
+
+    @Override
+    public void processPoint(Function<Integer, Double> action) {
+        int arm;
+        synchronized (getHolder()) {
+            if (tries < getArms()) {
+                arm = tries;
+            } else {
+                arm = IntStream.range(0, getArms())
+                        .mapToObj(i -> i)
+                        .sorted(Comparator.comparingDouble(i -> mu(i) + sqrt(2 * log(tries) / getVisitedNumber()[i])))
+                        .findFirst()
+                        .get();
+            }
+            ++getVisitedNumber()[arm];
+            ++tries;
+            //TODO: maybe update visitedSum temporarily while reward is not computed yet?
+        }
+        double reward = action.apply(arm);
+        synchronized (getHolder()) {
+            getVisitedSum()[arm] += reward;
+        }
+    }
+}
