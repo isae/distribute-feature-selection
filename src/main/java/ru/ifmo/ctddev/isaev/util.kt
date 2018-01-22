63e690107b424be16a156c86ff818c40f62434dc
iisaev <iisaev@solanteq.com>
2018-01-23 02:30:08 +0300

    migrated to kotlin even more

--- src/main/java/ru/ifmo/ctddev/isaev/util.kt
+++ src/main/java/ru/ifmo/ctddev/isaev/util.kt
@@ -1,9 +1,15 @@
 package ru.ifmo.ctddev.isaev
 
+import org.slf4j.LoggerFactory
 import ru.ifmo.ctddev.isaev.dataset.Feature
+import ru.ifmo.ctddev.isaev.dataset.FeatureDataSet
 import ru.ifmo.ctddev.isaev.feature.RelevanceMeasure
 import ru.ifmo.ctddev.isaev.folds.FoldsEvaluator
 import ru.ifmo.ctddev.isaev.result.Point
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileReader
 import java.lang.Double.compare
 
 /**
@@ -26,5 +32,43 @@ class SelectionResult(val selectedFeatures: List<Feature>,
 
     fun betterThan(bestScore: SelectionResult): Boolean {
         return this.compareTo(bestScore) == 1
+    }
+}
+
+class DataSetReader {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    fun readCsv(path: String): FeatureDataSet {
+        return readDataset(File(path), ",")
+    }
+
+    fun readCsv(file: File): FeatureDataSet {
+        return readDataset(file, ",")
+    }
+
+    private fun readDataset(file: File, delimiter: String): FeatureDataSet {
+        try {
+            val reader = BufferedReader(FileReader(file))
+            val counter = intArrayOf(0)
+            val goodLines = Sequence { reader.lines().iterator() }
+                    .filter { l -> !l.contains("NaN") }
+            val classes = parseRow(goodLines.first(), delimiter)
+            val features = goodLines
+                    .map { parseRow(it, delimiter) }
+                    .map { Feature("feature ${++counter[0]}", it) }
+            val featuresList = features.toList()
+            logger.debug("Read dataset by path {}; {} objects; {} features", arrayOf(file.absoluteFile, classes.size, featuresList.size))
+            return FeatureDataSet(featuresList, classes, file.name)
+        } catch (e: FileNotFoundException) {
+            throw IllegalArgumentException("File not found: " + file.name, e)
+        }
+
+    }
+
+    private fun parseRow(line: String, delimiter: String): List<Int> {
+        return line.split(delimiter.toRegex())
+                .dropLastWhile { it.isEmpty() }
+                .map({ Integer.valueOf(it) })
     }
 }
