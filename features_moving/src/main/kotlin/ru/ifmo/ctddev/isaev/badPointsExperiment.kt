1026af1a7b7979971fd648b8ca98c42fa5a87766
iisaev <iisaev@solanteq.com>
2018-05-17 14:03:53 +0300

    added failing experiment

--- features_moving/src/main/kotlin/ru/ifmo/ctddev/isaev/badPointsExperiment.kt
+++ features_moving/src/main/kotlin/ru/ifmo/ctddev/isaev/badPointsExperiment.kt
@@ -2,7 +2,6 @@ package ru.ifmo.ctddev.isaev
 
 import ru.ifmo.ctddev.isaev.feature.measure.VDM
 import ru.ifmo.ctddev.isaev.space.*
-import java.util.*
 
 
 /**
@@ -18,31 +17,31 @@ private val dataSet = KnownDatasets.DLBCL.read()
 private val evaluatedDataSet = evaluateDataSet(dataSet, measures)
 
 fun main(args: Array<String>) {
-    /* var lRaw = SpacePoint(intArrayOf(545), 2048)
-     var rRaw = SpacePoint(intArrayOf( 1091), 4096)*/
-    var lRaw = SpacePoint(longArrayOf(2048, 545), 2048)
-    var rRaw = SpacePoint(longArrayOf(4096, 1091), 4096)
+   /* var lRaw = SpacePoint(longArrayOf(2048, 545), 2048) //old experiment, success
+    var rRaw = SpacePoint(longArrayOf(4096, 1091), 4096)*/
+    var lRaw = SpacePoint(longArrayOf(64, 45), 64) //old experiment, success
+    var rRaw = SpacePoint(longArrayOf(256, 181), 256)
     do {
         println("Left: $lRaw")
         println("Right: $rRaw")
         val mRaw = inBetween(lRaw, rRaw)
         val left = getPointOnUnitSphere(getAngle(lRaw))
         val langles = getAngle(lRaw)
-        println("Left angles: ${Arrays.toString(langles)}")
+        //println("Left angles: ${Arrays.toString(langles)}")
         val rangles = getAngle(rRaw)
-        println("Right angles: ${Arrays.toString(rangles)}")
+        //println("Right angles: ${Arrays.toString(rangles)}")
         val right = getPointOnUnitSphere(rangles)
         val middle = getPointOnUnitSphere(getAngle(mRaw))
         val lCut = processPointGetWholeCut(left, evaluatedDataSet, cutSize)
         val rCut = processPointGetWholeCut(right, evaluatedDataSet, cutSize)
         val mCut = processPointGetWholeCut(middle, evaluatedDataSet, cutSize)
-        println("Lcut: $lCut")
-        println("Mcut: $mCut")
-        println("Rcut: $rCut")
+        //println("Lcut: $lCut")
+        //println("Mcut: $mCut")
+        //println("Rcut: $rCut")
         println("Diff between left and right: ${getDiff(lCut, rCut)}")
-        println("Diff between left and right, indices: ${getDiff(lCut, rCut).map { f -> lCut.indexOf(f) }}")
+        //println("Diff between left and right, indices: ${getDiff(lCut, rCut).map { f -> lCut.indexOf(f) }}")
         println("Diff between right and left: ${getDiff(rCut, lCut)}")
-        println("Diff between right and left, indices: ${getDiff(rCut, lCut).map { f -> rCut.indexOf(f) }}")
+        //println("Diff between right and left, indices: ${getDiff(rCut, lCut).map { f -> rCut.indexOf(f) }}")
         val lDiff = getDiff(lCut, mCut)
         println("Diff between left and middle: $lDiff")
         val rDiff = getDiff(mCut, rCut)
@@ -54,7 +53,7 @@ fun main(args: Array<String>) {
         println("Diff since $firstDiffPos; r: ${rCut.drop(firstDiffPos)}")
         println("For left diff: ")
         val targetDiff: List<Int>
-        if (lDiff.isEmpty()) {
+        if (lDiff.size < 2) {
             println("Going to the right")
             targetDiff = rDiff
             lRaw = mRaw
@@ -63,5 +62,5 @@ fun main(args: Array<String>) {
             targetDiff = lDiff
             rRaw = mRaw
         }
-    } while (targetDiff.size > 1)
+    } while (lDiff.size > 1 || rDiff.size > 1)
 }
