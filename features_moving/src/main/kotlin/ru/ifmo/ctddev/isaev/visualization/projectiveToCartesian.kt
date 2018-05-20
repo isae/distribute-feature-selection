21b1b2ca62425809a4240361bbe08adeea314085
iisaev <iisaev@solanteq.com>
2018-05-20 19:38:41 +0300

    cool visualizer

--- features_moving/src/main/kotlin/ru/ifmo/ctddev/isaev/visualization/projectiveToCartesian.kt
+++ features_moving/src/main/kotlin/ru/ifmo/ctddev/isaev/visualization/projectiveToCartesian.kt
@@ -1,28 +1,34 @@
 package ru.ifmo.ctddev.isaev
 
 import org.jzy3d.analysis.AbstractAnalysis
-import org.jzy3d.analysis.AnalysisLauncher
 import org.jzy3d.chart.Chart
+import org.jzy3d.chart.ChartLauncher.configureControllers
+import org.jzy3d.chart.Settings
 import org.jzy3d.chart.controllers.keyboard.camera.AWTCameraKeyController
 import org.jzy3d.chart.factories.AWTChartComponentFactory
 import org.jzy3d.colors.Color
 import org.jzy3d.colors.ColorMapper
 import org.jzy3d.colors.colormaps.ColorMapWhiteBlue
-import org.jzy3d.maths.*
+import org.jzy3d.maths.BoundingBox3d
+import org.jzy3d.maths.Coord3d
+import org.jzy3d.maths.Range
 import org.jzy3d.plot3d.builder.Builder
 import org.jzy3d.plot3d.builder.Mapper
 import org.jzy3d.plot3d.primitives.AbstractDrawable
 import org.jzy3d.plot3d.primitives.LineStrip
 import org.jzy3d.plot3d.primitives.Point
-import org.jzy3d.plot3d.primitives.textured.DrawableTexture
 import org.jzy3d.plot3d.rendering.canvas.Quality
 import org.jzy3d.plot3d.rendering.scene.Graph
-import org.jzy3d.plot3d.rendering.textures.BufferedImageTexture
-import org.jzy3d.plot3d.text.drawable.DrawableTextTexture
-import org.jzy3d.plot3d.text.drawable.TextImageRenderer
+import org.jzy3d.ui.MultiChartPanel
 import ru.ifmo.ctddev.isaev.space.getPointOnUnitSphere
-import java.awt.Font
+import java.awt.FlowLayout
+import java.awt.GridLayout
 import java.awt.event.KeyEvent
+import java.text.DecimalFormat
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JTextArea
 import kotlin.math.sqrt
 
 
@@ -36,10 +42,32 @@ private var phi = Math.PI / 3
 
 private var drawablesByPoint = emptyList<AbstractDrawable>()
 
+private val decimal = DecimalFormat("0.0000")
+
+val xTextArea = JTextArea("").apply {
+    isEditable = false
+}
+val yTextArea = JTextArea("").apply {
+    isEditable = false
+}
+val zTextArea = JTextArea("").apply {
+    isEditable = false
+}
+val thetaTextArea = JTextArea("").apply {
+    isEditable = false
+}
+val phiTextArea = JTextArea("").apply {
+    isEditable = false
+}
+
 fun main(args: Array<String>) {
+    Settings.getInstance().isHardwareAccelerated = true
     val scene = ProjectiveToEuclidean()
-    AnalysisLauncher.open(scene, Rectangle(0, 0, 1024, 768))
+    //AnalysisLauncher.open(scene, Rectangle(0, 0, 1024, 768))
+    scene.init()
     val chart = scene.chart
+    val mouse = configureControllers(chart, "ProjectiveToEuclidean", true, false)
+    chart.render()
     chart.addController(object : AWTCameraKeyController() {
         override fun keyPressed(e: KeyEvent) {
             if (e.isShiftDown) {
@@ -53,6 +81,35 @@ fun main(args: Array<String>) {
             }
         }
     })
+    val multiChart = MultiChartPanel(listOf(chart), false, 1150, 800).apply {
+        addPanel(JPanel(FlowLayout(FlowLayout.CENTER, 0, 0), true).apply {
+            add(
+                    JPanel(GridLayout(5, 1), true).apply {
+                        add(JLabel("X:"))
+                        add(JLabel("Y:"))
+                        add(JLabel("Z:"))
+                        add(JLabel("Theta:"))
+                        add(JLabel("Phi:"))
+                    }
+            )
+            add(
+                    JPanel(GridLayout(5, 1), true).apply {
+                        add(xTextArea)
+                        add(yTextArea)
+                        add(zTextArea)
+                        add(thetaTextArea)
+                        add(phiTextArea)
+                    }
+            )
+        })
+    }
+    JFrame().apply {
+        title = "ProjectiveToEuclidean"
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        add(multiChart)
+        pack()
+        isVisible = true
+    }
 }
 
 private fun updateDrawables(chart: Chart) {
@@ -108,41 +165,27 @@ private class ProjectiveToEuclidean : AbstractAnalysis() {
 private fun getDrawablesByPoint(theta: Double, phi: Double): List<AbstractDrawable> {
     val cartesian = getPointOnUnitSphere(doubleArrayOf(theta, phi))
     val pointOnSphere = Coord3d(cartesian.coordinates[0], cartesian.coordinates[1], cartesian.coordinates[2])
-    println("Current coordinates: phi = $phi, theta = $theta conversion from [$theta, $phi] to $cartesian")
+    //println("Current coordinates: phi = $phi, theta = $theta conversion from [$theta, $phi] to $cartesian")
     val pointOnX = Coord3d(pointOnSphere.x, 0.0f, 0.0f)
     val pointOnY = Coord3d(0.0f, pointOnSphere.y, 0.0f)
     val pointOnZ = Coord3d(0.0f, 0.0f, pointOnSphere.z)
-    val lineFromOrigin = getLineFromOrigin(pointOnSphere, Color.RED, 3.0)
-
-    fun makeMapping(dim: Coord2d): List<Coord2d> {
-        return DrawableTexture.getManualTextureMapping(dim.x, dim.y, dim.x / 2.0f, dim.y / 2.0f)
-    }
-
-    val xDescr = DrawableTextTexture(makeImage(pointOnSphere.x), PlaneAxis.X, pointOnSphere.x, makeMapping(Coord2d(0.1f, 0.1f)), Color.BLACK).apply {
-        // ???
-    }
-
-    val yDescr = DrawableTextTexture(makeImage(pointOnSphere.y), PlaneAxis.Y, pointOnSphere.y, makeMapping(Coord2d(0.1f, 0.1f)), Color.BLACK).apply {
-        // ???
-    }
-
-    val zDescr = DrawableTextTexture(makeImage(pointOnSphere.z), PlaneAxis.Z, pointOnSphere.z, makeMapping(Coord2d(0.1f, 0.1f)), Color.BLACK).apply {
-        // ???
-    }
-
     val projectionOnXY = Coord3d(pointOnSphere.x, pointOnSphere.y, 0.0f)
+    xTextArea.text = decimal.format(pointOnSphere.x)
+    yTextArea.text = decimal.format(pointOnSphere.y)
+    zTextArea.text = decimal.format(pointOnSphere.z)
+    phiTextArea.text = decimal.format(phi)
+    thetaTextArea.text = decimal.format(theta)
     return listOf(
-            lineFromOrigin,
+            getLineFromOrigin(pointOnSphere, Color.RED, 4.0),
             getLine(pointOnSphere, pointOnX, Color.RED, 1.0),
-            xDescr,
             getLine(pointOnSphere, pointOnY, Color.RED, 1.0),
-            yDescr,
             getLine(pointOnSphere, pointOnZ, Color.RED, 1.0),
-            zDescr,
             getLineFromOrigin(projectionOnXY, Color.RED, 1.0),
             getLine(pointOnSphere, projectionOnXY, Color.RED, 1.0),
             getLine(pointOnX, projectionOnXY, Color.RED, 1.0),
-            getLine(pointOnY, projectionOnXY, Color.RED, 1.0)
+            getLine(pointOnY, projectionOnXY, Color.RED, 1.0),
+            getLine(pointOnX, pointOnZ, Color.RED, 1.0),
+            getLine(pointOnY, pointOnZ, Color.RED, 1.0)
     )
 }
 
@@ -157,9 +200,4 @@ private fun getLine(from: Coord3d, to: Coord3d, color: Color, width: Double): Li
             }
     result.setWidth(width.toFloat())
     return result
-}
-
-fun makeImage(text: Number): BufferedImageTexture {
-    val font = Font("Serif", Font.BOLD, 12)
-    return TextImageRenderer(text.toString(), font).image
 }
